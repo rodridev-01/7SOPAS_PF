ffd062808a7e1b2f5585373751732a6d558450c0
rodridev-01 <rph26031994@gmail.com>
2026-05-02 22:57:09 -0500

    public

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
-const path = require('path');
-app.use(express.static(path.join(__dirname, '.')))
-app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Rutas API
 app.use("/api/usuarios", require("./routes/usuarios"));
 app.use("/api/productos", require("./routes/productos"));
 app.use("/api/sedes", require("./routes/sedes"));
 
-app.listen(3000, "127.0.0.1", () => {
-  console.log("Servidor en http://127.0.0.1:3000");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-app.get('/', (req, res) => {
-  console.log(__dirname);
-    res.sendFile(path.join(__dirname, '../index.html'));
+
+app.get("/:page", (req, res, next) => {
+  const filePath = path.join(__dirname, "../public", req.params.page + ".html");
+
+  res.sendFile(filePath, (err) => {
+    if (err) next(); 
+  });
 });
-app.get('/menu', (req, res) => {
-  console.log(__dirname);
-    res.sendFile(path.join(__dirname, '../menu.html'));
-}); 
-app.get('/login', (req, res) => {
-  console.log(__dirname);
-    res.sendFile(path.join(__dirname, '../login.html'));
-}); 
-app.get('/register', (req, res) => {
-  console.log(__dirname);
-    res.sendFile(path.join(__dirname, '../register.html'));
-}); 
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
+});
